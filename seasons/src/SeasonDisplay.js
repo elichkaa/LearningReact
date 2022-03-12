62d2496da75fb337ef1b697dc491ec18b09dcc7c
elichka <ed44362072@edu.mon.bg>
2022-03-12 21:19:17 +0200

    Finish lecture 6

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
-const SeasonDisplay = () => {
-	return <div>Season display</div>;
+const seasonConfig = {
+	summer: {
+		text: "So hot...",
+		icon: "sun",
+	},
+	winter: {
+		text: "It is freezing",
+		icon: "snowflake",
+	},
+};
+
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? "summer" : "winter";
+	} else {
+		return lat > 0 ? "winter" : "summer";
+	}
+};
+
+const SeasonDisplay = (props) => {
+	const season = getSeason(props.lat, new Date().getMonth());
+	const { text, icon } = seasonConfig[season];
+	return (
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left massive ${icon} icon`}></i>
+			<h1>{text}</h1>
+			<i className={`icon-right massive ${icon} icon`}></i>
+		</div>
+	);
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { lat: null, errorMessage: "" };
+	}
 
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => {
 				this.setState({ lat: position.coords.latitude });
@@ -21,9 +25,9 @@ class App extends React.Component {
 			return <div>Error: {this.state.errorMessage}</div>;
 		}
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
-		return <div>Loading!</div>;
+		return <Loader message="Please accept location request" />;
 	}
 }
 
