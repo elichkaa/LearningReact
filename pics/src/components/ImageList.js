568e5b036510a863da91eed97d3a92aa0525d9e6
elichka <ed44362072@edu.mon.bg>
2022-03-13 12:15:26 +0200

    Data visualization (lecture 9)

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ImageList = (props) => {
+	return props.images.map((image) => {
+		return (
+			<img
+				key={image.id}
+				src={image.urls.regular}
+				alt={image.description}
+			/>
+		);
+	});
+};
+
+export default ImageList;
