1e9dfec60d18cebc70dfbb2fd38f084d715d7e83
elichka <ed44362072@edu.mon.bg>
2022-03-13 09:11:02 +0200

    Event handling (lecture 7)

---
+++ pics/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+// import Validator from "./Validator";
+
+const App = () => {
+	return (
+		<div className="ui container" style={{ marginTop: "10px" }}>
+			<SearchBar />
+			{/* <Validator /> */}
+		</div>
+	);
+};
+
+export default App;
