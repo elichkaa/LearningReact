9a066638e2d8594eb4faff1fd32cb7eccf45b3d6
elichka <ed44362072@edu.mon.bg>
2022-03-26 09:54:30 +0200

    Complete custom hook

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,31 +1,22 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
-import youtube from "../apis/youtube";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-	const [videos, setVideos] = useState([]);
 	const [selectedVideo, setSelectedVideo] = useState(null);
 
-	const onTermSubmit = async (term) => {
-		const response = await youtube.get("/search", {
-			params: {
-				q: term,
-			},
-		});
+	const [videos, search] = useVideos("nijisanji");
 
-		setVideos(response.data.items);
-		setSelectedVideo(response.data.items[0]);
-	};
-
+	//every time videos change
 	useEffect(() => {
-		onTermSubmit("nijisanji");
-	}, []);
+		setSelectedVideo(videos[0]);
+	}, [videos]);
 
 	return (
 		<div className="ui container">
-			<SearchBar onFormSubmit={onTermSubmit} />
+			<SearchBar onFormSubmit={search} />
 			<div className="ui grid">
 				<div className="ui row">
 					<div className="eleven wide column">
--- videos/src/hooks/useVideos.js
+++ videos/src/hooks/useVideos.js
@@ -1,9 +1,24 @@
 import { useState, useEffect } from "react";
 import youtube from "../apis/youtube";
 
-const useVideos = () => {
-    const [videos, setVideos] = useState([]);
-    
+const useVideos = (defaultSearchTerm) => {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		search(defaultSearchTerm);
+	}, [defaultSearchTerm]);
+
+	const search = async (term) => {
+		const response = await youtube.get("/search", {
+			params: {
+				q: term,
+			},
+		});
+
+		setVideos(response.data.items);
+	};
+
+	return [videos, search];
 };
 
 export default useVideos;
