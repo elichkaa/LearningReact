7024d8b0be17f4ef84610cd57504e958beb3dff4
elichka <ed44362072@edu.mon.bg>
2022-03-26 09:47:31 +0200

    Refactor class components to hook ones from videos and create a custom hook

---
+++ videos/src/hooks/useVideos.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+const useVideos = () => {
+    const [videos, setVideos] = useState([]);
+    
+};
+
+export default useVideos;
